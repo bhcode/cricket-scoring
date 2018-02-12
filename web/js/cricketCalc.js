1f8f0afaa6c98a898901638b78b776e3510fccda
bhcode <brittney.huxford@gmail.com>
2018-02-12 13:36:51 +1300

    change

--- web/js/cricketCalc.js
+++ web/js/cricketCalc.js
@@ -13,13 +13,13 @@ function enter(key) {
     document.getElementById('score').innerHTML = score;
 }
 
-/**
+/** 
     Removes the last entry from  the scorecard, and subtracts it from the total score.
 */
 function backspace() {
     var previousBall = scorecard.charAt(scorecard.length - 1);
     score = score - previousBall;
-    scorecard = scorecard.substring(0, scorecard.length);
+    scorecard = scorecard.substring(0, scorecard.length - 1);
     document.getElementById('scorecard').innerHTML = scorecard;
     document.getElementById('score').innerHTML = score;
 }
